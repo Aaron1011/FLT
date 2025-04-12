0d854f3526de20a00fa8998abec09f55724b1b14
Aaron Hill <aa1ronham@gmail.com>
2025-04-12 16:17:10 -0400

    Get something working

--- FLT/HaarMeasure/DistribHaarChar/Padic.lean
+++ FLT/HaarMeasure/DistribHaarChar/Padic.lean
@@ -34,29 +34,73 @@ variable {p : ℕ} [Fact p.Prime]
 private lemma distribHaarChar_padic_padicInt (x : ℤ_[p]⁰) :
     distribHaarChar ℚ_[p] (x : ℚ_[p]ˣ) = ‖(x : ℚ_[p])‖₊ := by
   -- Let `K` be the copy of `ℤ_[p]` inside `ℚ_[p]` and `H` be `xK`.
-  let K : AddSubgroup ℚ_[p] := (1 : Submodule ℤ_[p] ℚ_[p]).toAddSubgroup
-  let H := (x : ℚ_[p]) • K
+  let K : AddSubgroup ℤ_[p] := (⊤ : Submodule ℤ_[p] ℤ_[p]).toAddSubgroup
+  let H := (x.val • (1 : Submodule ℤ_[p] ℤ_[p])).toAddSubgroup
   -- We compute that `volume H = ‖x‖₊ * volume K`.
-  refine distribHaarChar_eq_of_measure_smul_eq_mul (s := K) (μ := volume) (G := ℚ_[p]ˣ)
-    (by simp [K, Padic.submodule_one_eq_closedBall, closedBall, Padic.volume_closedBall_one])
-    (by simp [K, Padic.submodule_one_eq_closedBall, closedBall, Padic.volume_closedBall_one]) ?_
-  change volume (H : Set ℚ_[p]) = ‖(x : ℚ_[p])‖₊ * volume (K : Set ℚ_[p])
-  -- This is true because `H` is a `‖x‖₊⁻¹`-index subgroup of `K`.
+  -- refine distribHaarChar_eq_of_measure_smul_eq_mul (s := K) (μ := volume) (G := ℚ_[p]ˣ)
+  --   (by simp [K, Padic.submodule_one_eq_closedBall, closedBall, Padic.volume_closedBall_one])
+  --   (by simp [K, Padic.submodule_one_eq_closedBall, closedBall, Padic.volume_closedBall_one]) ?_
+  -- change volume (H : Set ℚ_[p]) = ‖(x : ℚ_[p])‖₊ * volume (K : Set ℚ_[p])
+  -- -- This is true because `H` is a `‖x‖₊⁻¹`-index subgroup of `K`.
   have hHK : H ≤ K := by
     simpa [H, K, -Submodule.smul_le_self_of_tower]
       using (1 : Submodule ℤ_[p] ℚ_[p]).smul_le_self_of_tower (x : ℤ_[p])
-  have : H.FiniteRelIndex K :=
-    PadicInt.smul_submodule_finiteRelIndex (p := p) (mem_nonZeroDivisors_iff_ne_zero.1 x.2) 1
-  have H_relindex_Z : (H.relindex K : ℝ≥0∞) = ‖(x : ℚ_[p])‖₊⁻¹ :=
-    congr(ENNReal.ofNNReal $(PadicInt.smul_submodule_relindex (p := p) x 1))
-  rw [← index_mul_addHaar_addSubgroup_eq_addHaar_addSubgroup hHK, H_relindex_Z, ENNReal.coe_inv,
-    ENNReal.mul_inv_cancel_left]
-  · simp
-  · simp
-  · simp
-  · simpa [H, K, Padic.submodule_one_eq_closedBall]
-      using measurableSet_closedBall.const_smul (x : ℚ_[p]ˣ)
-  · simpa [K, Padic.submodule_one_eq_closedBall] using measurableSet_closedBall
+  --have : H.FiniteRelIndex K :=
+  --  PadicInt.smul_submodule_finiteRelIndex (p := p) (mem_nonZeroDivisors_iff_ne_zero.1 x.2) 1
+  have H_relindex_Z : (H.relindex K : ℝ≥0∞) = ‖(x : ℚ_[p])‖₊⁻¹ := by
+    have x_nonzero: x.val ≠ 0 := by
+      exact nonZeroDivisors.ne_zero x.prop
+
+    have bar := PadicInt.mem_span_pow_iff_le_valuation x x_nonzero (p := p)
+    let target_ideal := Ideal.span {↑p ^ x.val.valuation}
+
+
+    have x_factor := PadicInt.unitCoeff_spec x_nonzero
+    norm_cast at x_factor
+    unfold H
+    nth_rw 1 [x_factor]
+    conv =>
+      lhs
+      arg 1
+      arg 1
+      arg 1
+      equals (Ideal.span {(p: ℤ_[p]) ^ (x.val.valuation)}) =>
+        let a := 1
+        ext y
+        refine ⟨?_, ?_⟩
+        . intro hy
+          simp at hy
+          rw [← Submodule.singleton_set_smul] at hy
+          rw [Submodule.mem_singleton_set_smul] at hy
+          obtain ⟨m, ⟨m_top, hm⟩⟩ := hy
+          rw [Ideal.mem_span_singleton']
+          simp at hm
+          rw [mul_comm] at hm
+          rw [← mul_assoc] at hm
+          exact Exists.intro (m * ↑(PadicInt.unitCoeff x_nonzero)) (id (Eq.symm hm))
+
+        . intro hy
+        done
+
+
+
+    sorry
+  sorry
+
+
+
+
+
+
+  --   --congr(ENNReal.ofNNReal $(PadicInt.smul_submodule_relindex (p := p) x 1))
+  -- rw [← index_mul_addHaar_addSubgroup_eq_addHaar_addSubgroup hHK, H_relindex_Z, ENNReal.coe_inv,
+  --   ENNReal.mul_inv_cancel_left]
+  -- · simp
+  -- · simp
+  -- · simp
+  -- · simpa [H, K, Padic.submodule_one_eq_closedBall]
+  --     using measurableSet_closedBall.const_smul (x : ℚ_[p]ˣ)
+  -- · simpa [K, Padic.submodule_one_eq_closedBall] using measurableSet_closedBall
 
 /-- The distributive Haar character of the action of `ℚ_[p]ˣ` on `ℚ_[p]` is the usual p-adic norm.
 
